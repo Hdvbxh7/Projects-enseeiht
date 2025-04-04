1c7b3c3cf040617d543ea8b16b30bf82675a154b
Tom <tom.gutierrez1040@gmail.com>
2025-04-04 17:17:44 +0200

    maj

--- 2A/prolog/projet/cube.pl
+++ 2A/prolog/projet/cube.pl
@@ -23,27 +23,13 @@ clear(X).
 
 r_put_on(X,Y):-clear(X),clear(Y),put_on(X,Y).
 
+%tri
+triee(Z,[]):-true.
+triee(on(X1,Y1),[on(X2,Y2)|T]):-Y1\=X2,triee(on(X1,Y1),T).
+triee([on(X,Y)|T]):-triee(on(X,Y),T),triee(T).
+
 /*si on lui demande de le faire en l'état, comme c n'est plus libre à la fin, on n'obtient pas la bonne configuration à la fin*/
 achieven([on(X,Y)]):-r_put_on(X,Y),listing(move),retractall(move(_,_)).
 achieven([on(X,Y)|T]):-r_put_on(X,Y),achieven(T).
-achievef([on(X,Y)]):-retractall(move(_,_)),r_put_on(X,Y),listing(move),retractall(move(_,_)).
-achievef([on(X,Y)|T]):-retractall(move(_,_)),r_put_on(X,Y),achieven(T).
-
-
-
-/*version bonne configuation avec sort*/
-add([on(X2,Y2)],[],P):-P = [on(X2,Y2)].
-add([on(X2,Y2)|T2],[],P):-add(T2,F,Z),P = [on(X2,Y2)|Z].
-add(Z,F,P):-add(Z,[],D),P=[F|D].
-
-sort(D,[],P):- P = [D].
-sort(on(X,Y),Z,P):-Z=[on(X2,Y2)|T2],X==Y2,add(Z,on(X,Y),P).
-sort(on(X,Y),[on(X2,Y2)|T2],P):-sort(on(X,Y),T2,Z),P=[on(X2,Y2)|Z].
-
-sortx([],Z,Y).
-sortx([on(X,Y)|T],[],S):-sortx(T,[on(X,Y)],S).
-sortx([on(X1,Y1)|T1],Z,S):-sort(on(X1,Y1),Z,E),D=E,F=E,sortx(T1,F,D),S=D.
-
-%je pense que le probléme vient du D=E qui n'est plus vrai quand le programme a fini le sort qui casse tout mais je n'ai pas réussi à le résoudre%
-
-achieve(X):-sortx(X,Z),achievef(Z).
+achieve([on(X,Y)]):-retractall(move(_,_)),r_put_on(X,Y),listing(move),retractall(move(_,_)).
+achieve(L1):-retractall(move(_,_)),permutation(L1,L2),triee(L2),achieven(L2).
